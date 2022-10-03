5a09b81cdef483d183b362fd91714f50b10231f9
maxlubov <maxim.lubov@opensesame.com>
2022-10-03 10:48:32 +0500

    Update project data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
-ti = time()
+using HNSWm
+using Test
 
-include("lowlevel_tests.jl")
-include("compare_nearestneighbors.jl")
-ti = time() - ti
-println("\nTest took total time of:")
-println(round(ti, digits=3), " seconds or ", round(ti/60, digits=3), " minutes")
+@testset "HNSWm.jl" begin
+    # Write your tests here.
+end

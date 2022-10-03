12538e8a5474ab8a170c084c90c4bb0609ba6a27
maxlubov <maxim.lubov@opensesame.com>
2022-10-03 11:10:41 +0500

    Update knn_search

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -90,7 +90,6 @@ end
 function knn_search(hnsw, q, K)
     ef = max(K, hnsw.ef)
     # @assert length(q)==length(hnsw.data[1])
-    print(hnsw)
     ep = get_enter_point(hnsw)
     epN = Neighbor(ep, distance(hnsw, q, ep))
     L = get_entry_level(hnsw) #layer of ep , top layer of hnsw
